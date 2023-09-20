1eb0e9ca63dbf58b67b59014f2f3500da4a8e9fd
Aurèle Oulès <aurele@oules.com>
2023-09-20 12:30:17 +0000

    Build docker image

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -2,6 +2,7 @@ import cookie from 'cookie';
 
 export async function handle({ event, resolve }) {
 	const session = cookie.parse(event.request.headers.get('cookie') || '');
+	console.log(session);
 	event.locals.session = session;
 
 	return await resolve(event);
