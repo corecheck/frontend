5c1a45fa4cfa6d4d6b977e5880e9363dfe64fc5e
Aurele Oules <aurele@oules.com>
2023-12-16 22:34:44 +0100

    Cleanup unused routes

--- src/routes/login/+page.ts
+++
@@ -1,8 +0,0 @@
-import { env } from '$env/dynamic/public'
-import { redirect } from "@sveltejs/kit";
-
-const CLIENT_ID = env.PUBLIC_CLIENT_ID;
-
-export function load() {
-	throw redirect(307, `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${env.PUBLIC_REDIRECT_URL}`);
-}
